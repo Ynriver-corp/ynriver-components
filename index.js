1d454eaf41b8b2f50b1a9fe52c278aa98c046571
Pablo Yncio R <pablo.yncio@immertec.com>
2022-04-24 16:29:33 -0500

    add TODO

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const Test = (value) => value === "ynriver-1.0.1";
 
 module.exports = {Test};
+
+// TODO: Install antd.
+// TODO: BTN.
+// TODO: ANCHOR.
+// TODO: INPUT.
+// TODO: React theme from json.
+
+// TODO: useTranslation.
+// TODO: useSendError.
+// TODO: useFetch.
+// TODO: useAuth.
+
+// TODO: Transpile before to publish.
