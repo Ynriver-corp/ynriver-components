95228af82fdacb8547c4fd1762a7bb3d02a582df
Pablo Yncio R <pablo.yncio@immertec.com>
2022-04-24 22:53:20 -0500

    test v1

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //import * as Button from "./src/components/form/Button"
 
-//module.exports = require("./dist/index.js")
+module.exports = require("./dist/index")
 
-const {Test} = require("./src/components/commons/Anchor");
-const Button = () => <button>hola</button>
+//const {Test} = require("./src/components/commons/Anchor");
+//const Button = () => <button>hola</button>
 
-module.exports = {Test, Button};
+//module.exports = {Test, Button};
 
