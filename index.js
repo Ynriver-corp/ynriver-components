a4f5da98a93e5bb60de1bc5b2071dbe713cce8b0
Pablo Yncio R <pablo.yncio@immertec.com>
2022-04-24 23:16:15 -0500

    test v1

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-import React from "react";
 //import * as Button from "./src/components/form/Button"
 
-//module.exports = require("./dist/index.js")
-
+module.exports = require("./dist/index.js")
+/*
 const {Test} = require("./src/components/commons/Anchor");
 const Button = () => <button>hola</button>
 
 module.exports = {Test, Button};
-
+*/
