2e48244c5ad95e39e43310c56f281dbf82f5d426
Pablo Yncio R <pablo.yncio@immertec.com>
2022-04-25 23:35:53 -0500

    update bundle

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-//import React from "react";
+import React from "react";
 
 //export const {Test} = require("./components/commons/Anchor");
-const Test = (value) => value === "ynriver";
-export default Test;
+export const Test = (value) => value === "ynriver";
+//export default Test;
 
 //export const Button = () => <button>hola</button>
 //export default Button;
